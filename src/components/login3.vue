7251eaaa27d491cba83118c9eb2a55d4e7851ac5
Tunghk <tung1999zxc@gmail.com>
2023-07-28 15:52:46 +0700

    reset

--- src/components/login3.vue
+++ src/components/login3.vue
@@ -1,5 +1,21 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 11111111111111111111111222222
 =======
 11111111111111111111
 >>>>>>> parent of 7286bde (1)
+=======
+<template lang="">
+    <div>
+        
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="" scoped>
+    
+</style>
+>>>>>>> parent of f178555 (Revert "sua file login3")
